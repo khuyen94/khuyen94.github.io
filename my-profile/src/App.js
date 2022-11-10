1e2fb69ea747b66b2e43adf953d03f902c3f2870
Khuyen Tran <khuyen@dws.cloud>
2022-11-10 11:31:20 +0700

    test CI

--- my-profile/src/App.js
+++ my-profile/src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://khuyen94.github.io"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Home Page
         </a>
       </header>
     </div>
